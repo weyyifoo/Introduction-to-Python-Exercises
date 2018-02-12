160d74d2ca4670ca41c7da44bfcd3a1a1c4a0300
Weyyi <fwy2187@gmail.com>
2018-02-12 08:12:29 -0600

    1

--- Chapter7-ObjectsAndClasses/TV Remote/TV Control.py
+++ Chapter7-ObjectsAndClasses/TV Remote/TV Control.py
@@ -7,8 +7,8 @@ Created on Mon Feb 12 08:02:09 2018
 
 class TV:
     def __init__(self):
-        self.channel = channel # default channel is 1
-        self.volumelevel = volumelevel # default volume is 1
+        self.channel = 1 # default channel is 1
+        self.volumelevel = 1 # default volume is 1
         self.on = False # initially TV is off
     
     def turnon(self):
@@ -18,4 +18,37 @@ class TV:
         self.on = False
         
     def getchannel(self):
-        
+        return self.channel
+    
+    def setchannel(self, channel):
+        if (self.on is True) and (1 <= self.channel <= 200):
+            self.channel = channel
+            
+    def setvolume(self, volumelevel):
+        if (self.on is True) and (1 <= self.volumelevel <= 100):
+            self.volumelevel = volumelevel
+            
+    def channelup(self):
+        if (self.on is True) and (self.channel < 120):
+            self.channel += 1
+            
+    def channeldown(self):
+        if (self.on is True) and (self.channel > 1):
+            self.channel -= 1
+    
+    def volumeup(self):
+        if (self.volumelevel) < 100:
+            self.volumelevel += 1
+    
+    def volumedown(self):
+        if (self.volumelevel) > 1:
+            self.volumelevel -= 1
+            
+def main():
+    tv1 = TV()
+    tv1.turnon()
+    tv1.setchannel(50)
+    tv1.setvolume(20)
+    print(tv1.getchannel())
+    
+main()
